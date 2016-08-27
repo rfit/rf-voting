44af567734d2748c3f2287dd142a34f360dddc39
Elias Jørgensen <eliaszakarias@gmail.com>
2016-08-27 18:27:52 +0200

    Create selectable buttons

--- src/components/Poll/Poll.component.js
+++ src/components/Poll/Poll.component.js
@@ -7,18 +7,54 @@ export class Poll extends Component {
     super()
     // set initial state
     this.state = {
-      'selectedItem': 0
+      'selectedItems': []
+    }
+  }
+
+  handleClick (number) {
+    const index = this.state.selectedItems.indexOf(number)
+    if (index >= 0) {
+      // create new array
+      let newArray = this.state.selectedItems.slice(0, index)
+      newArray = newArray.concat(this.state.selectedItems.slice(index + 1))
+      // set new state
+      this.setState({
+        selectedItems: newArray
+      })
+    } else if (index === -1) {
+      // create new array
+      const newArray = this.state.selectedItems.concat(number)
+      // set new state
+      this.setState({
+        selectedItems: newArray
+      })
     }
   }
 
   render () {
+    console.log('STATE:', this.state)
     return (
       <div>
         <h1>This is RF Poll!</h1>
         <p>Take a look at our project thingies:</p>
         {
           this.props.items.map((item, key) => {
-            return <PollItem name={item.name} key={key} />
+            let selected = false
+            if (this.state.selectedItems.indexOf(key) > -1) {
+              selected = true
+            }
+
+            return (
+              <div>
+                <PollItem
+                  name={item.name}
+                  key={key}
+                  number={key}
+                  onButtonClick={(e) => this.handleClick(e)} // eslint-disable-line
+                  selected={selected} />
+                <br />
+              </div>
+            )
           })
         }
       </div>
--- src/components/PollItem/PollItem.component.js
+++ src/components/PollItem/PollItem.component.js
@@ -2,14 +2,22 @@ import React, {Component, PropTypes} from 'react'
 
 export class PollItem extends Component {
   render () {
+    const number = this.props.number
+    let style = {}
+    if (this.props.selected) { style = {'backgroundColor': 'grey'} }
+
+    /* eslint-disable */
     return (
-      <div>
+      <button onClick={() => this.props.onButtonClick(number)} style={style}>
         <h2>{this.props.name}</h2>
-      </div>
+      </button>
     )
   }
 }
 
 PollItem.propTypes = {
-  name: PropTypes.string.isRequired
+  name: PropTypes.string.isRequired,
+  selected: PropTypes.bool.isRequired,
+  onButtonClick: PropTypes.func.isRequired,
+  number: PropTypes.number.isRequired
 }
