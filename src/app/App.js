9fab8d44f75908a0afeff7aa4c7ecc59a2b173a1
Elias Jørgensen <eliaszakarias@gmail.com>
2016-09-03 11:35:45 +0200

    Rearrange folder structure to allow more client side related folders

--- src/app/App.js
+++ src/app/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import {Container} from './Container/Container'
+import {Container} from './components/Container/Container'
 
 // dummy project data
 const items = [
